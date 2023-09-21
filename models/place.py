7fc9f0b945d29f2b6c0c5b0e2bf4cc6e909e1235
Godwin Okpe <okpegodwin18@yahoo.com>
2023-09-21 11:22:34 -0700

    Feature: Implemented the Review table, and integrated with place and user tables. All tested and working well with the database

--- models/place.py
+++ models/place.py
@@ -2,29 +2,30 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
 from models import storage_type
 
 
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
 
     if storage_type == "db":
         city_id = Column(String(60), ForeignKey('cities.id'),
-                                      nullable=False)
-        user_id = city_id = Column(String(60), ForeignKey('users.id'),
-                                                      nullable=False)
+                         nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'),
+                         nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude =  Column(Float, nullable=True)
+        latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place",
+                               cascade="all, delete-orphan")
 
-        
     else:
         city_id = ""
         user_id = ""
@@ -37,3 +38,14 @@ class Place(BaseModel):
         latitude = 0.0
         longitude = 0.0
         amenity_ids = []
+
+        @property
+        def reviews(self):
+            """Get reviews associated with a place"""
+            from models import storage
+            place_reviews = []
+            all_reviews = storage.all(Review)
+            for item in all_reviews.values():
+                if item.place_id == self.id:
+                    place_reviews.append(item)
+            return place_reviews
